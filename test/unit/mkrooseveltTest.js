7787b4e1b09f0cf8f132eb9b8541c52c1c7881f9
Oumar Ly <lyomrau@gmail.com>
2019-10-08 17:19:16 -0400

    add test and assert pass

---
+++ test/unit/mkrooseveltTest.js
@@ -0,0 +1,8 @@
+/* eslint-env mocha */
+const assert = require('assert')
+
+describe('Create Roosevelt App', function () {
+  describe('To be added later', function () {
+    assert.strictEqual(true, true)
+  })
+})
