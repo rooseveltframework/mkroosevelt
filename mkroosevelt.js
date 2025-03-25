10a0a50f89a0cb835a5a539af4485b9a565bef60
Eric Newport <kethinov@gmail.com>
2025-03-25 10:11:46 -0400

    0.29.0

--- mkroosevelt.js
+++ mkroosevelt.js
@@ -1,45 +1,25 @@
 #!/usr/bin/env node
-
-const yeoman = require('yeoman-environment')
-const env = yeoman.createEnv()
-
-const myArgs = process.argv.slice(2)
-let chosenDirectoryName
-
-async function askDirectoryName () {
-  const inquirerModule = await import('inquirer')
-  const inquirer = inquirerModule.default
-
-  return inquirer
-    .prompt([
-      {
-        name: 'file',
-        type: 'input',
-        message: 'What would you like to name your Roosevelt app?',
-        default: 'my-roosevelt-sample-app'
-      }
-    ])
-    .then((response) => {
-      chosenDirectoryName = response.file
-    })
-}
-
-async function callYeoman (filename) {
-  // search for generator
-  await env.lookup()
-  env.run(`roosevelt --standard-install ${filename}`)
-}
-
-async function runGenerator () {
-  // check if args specified
-  if (myArgs.length < 1 || myArgs === undefined) {
-    // run prompt when no directory given
-    await askDirectoryName()
-  } else {
-    chosenDirectoryName = myArgs[0]
+(async () => {
+  const yeomanEnvModule = await import('yeoman-environment')
+  const env = new yeomanEnvModule.default()
+  const args = process.argv.slice(2)
+  let chosenDirectoryName
+  if (args.length > 0 && args !== undefined) chosenDirectoryName = args[0]
+  else {
+    const inquirerModule = await import('inquirer')
+    await inquirerModule.default
+      .prompt([
+        {
+          name: 'file',
+          type: 'input',
+          message: 'What would you like to name your Roosevelt app?',
+          default: 'my-roosevelt-sample-app'
+        }
+      ])
+      .then((response) => {
+        chosenDirectoryName = response.file
+      })
   }
-
-  callYeoman(chosenDirectoryName)
-}
-
-runGenerator()
+  await env.lookup()
+  await env.run(`roosevelt --standard-install ${chosenDirectoryName}`)
+})()
